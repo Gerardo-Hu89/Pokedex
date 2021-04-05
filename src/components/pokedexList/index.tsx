8b1ec2a32f143b517293c157129b49382ba963e6
Gerardo Huerta <81823448+Gerardo-Hu89@users.noreply.github.com>
2021-04-05 17:39:25 -0500

    Add files via upload

---
+++ src/components/pokedexList/index.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect, ReactElement } from 'react';
+
+type PokeBasic = {
+  [key: string]: string;
+};
+
+type RowProps = {
+  obj: any;
+  ind: number
+};
+
+const PokemonRow = ({obj, ind}: RowProps): ReactElement => {
+  const [pokeImg, setPokeImg] = useState<string>();
+
+  useEffect(() => {
+    (async function() {
+      const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${obj.name}`);
+      const pokemonImg = await data.json();
+      setPokeImg(pokemonImg.sprites.front_default);
+    })();
+  }, [obj.name]);
+
+  return (
+    <div>
+      <div>{obj.name}</div>
+      <div>
+        <img alt={obj.name} src={pokeImg} />
+      </div>
+    </div>
+  );
+};
+
+const PokedexList: React.FC<any> = (): ReactElement => {
+  const [data, setData] = useState<any>([]);
+  const [page, setPage] = useState<number>(0);
+  const [limit, setLimit] = useState<number>(10);
+
+  useEffect(() => {
+    (async function () {
+      const data = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=${limit}`);
+      const pokemons = await data.json();
+      setData(pokemons.results);
+    })();
+  }, [limit, page]);
+
+  return (
+    <div>
+      {data && data.map((el: PokeBasic[], i: number) => {
+        return <PokemonRow obj={el} ind={i} />
+      })}
+    </div>
+  );
+};
+
+export default PokedexList;
---
+++ src/pages/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PokedexList from '../components/pokedexList';
+import { Layout, Menu } from 'antd';
+import { MenuUnfoldOutlined, MenuFoldOutlined, UserOutlined, VideoCameraOutlined, UploadOutlined, } from '@ant-design/icons';
+import './styles.css';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const Pokedex = (): React.ReactElement => {
+  const [collapsed, setCollapsed] = React.useState<boolean>(false);
+
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            Pokedex
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            Search
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            To define
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: () => setCollapsed(!collapsed),
+          })}
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          <PokedexList />
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Pokedex;
